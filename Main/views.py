74a0ce1bf9049e4a23c3e6bc08be546d1c73ccce
EnriqueBBCode <142163184+EnriqueBBCode@users.noreply.github.com>
2024-01-31 15:01:06 -0500

    captcha JJJ

--- Main/views.py
+++ Main/views.py
@@ -9,7 +9,6 @@ import json, smtplib
 from django.core.mail import EmailMessage
 from django import forms
 from django_recaptcha.fields import ReCaptchaField
-from captcha.widgets import ReCaptchaV3
 
 @csrf_exempt
 def translate(request,lang):
@@ -32,42 +31,36 @@ def suscribe(request):
     Contact.objects.create(email=email)
     return HttpResponse(json.dumps(email),content_type='aplication/json')
 
-class FormWithCaptcha(forms.Form):
-    captcha = ReCaptchaField()
-    
 def contact(request):
     print(request.method)
     if request.method == "POST":
-        recaptcha_response = request.POST.get('g-recaptcha-response')
-        recaptcha_score = request.POST.get('g-recaptcha-score')
-        RECAPTCHA_PRIVATE_KEY = 'tu_clave_secreta'
-        recaptcha = ReCaptchaV3(re_captcha_secret=RECAPTCHA_PRIVATE_KEY, score_threshold=0.5)
+        
+        message = request.POST['msg']
+        tel = request.POST['phone']
+        name = request.POST['name']
+        email = request.POST['email']
+        Contacting.objects.create(
+            name = name,
+            email = email,
+            phone = tel,
+            msg = message
+        )
+        msg = f"""Name / Nombre\n{name}\nPhone / Teléfono\n{tel}\nEmail\n{email}\n\n{message}"""
+        emails = []
+        for m in Manager.objects.all():
+            emails.append(m.email)
+        email = EmailMessage(
+            f'New Contact / Nuevo Contacto',
+            msg,
+            "sendertest@godjango.dev",
+            emails
+        )
+        email.send()
+        print('mail sent')
+    return redirect('home')
 
-        if recaptcha.verify(request, recaptcha_response, recaptcha_score):
-            print("F2")
-            message = request.POST['msg']
-            tel = request.POST['phone']
-            name = request.POST['name']
-            email = request.POST['email']
-            Contacting.objects.create(
-                name = name,
-                email = email,
-                phone = tel,
-                msg = message
-            )
-            msg = f"""Name / Nombre\n{name}\nPhone / Teléfono\n{tel}\nEmail\n{email}\n\n{message}"""
-            emails = []
-            for m in Manager.objects.all():
-                emails.append(m.email)
-            email = EmailMessage(
-                f'New Contact / Nuevo Contacto',
-                msg,
-                "sendertest@godjango.dev",
-                emails
-            )
-            email.send()
-            print('mail sent')
-        return redirect('home')
+class FormWithCaptcha(forms.Form):
+    captcha = ReCaptchaField()
 
 class HomeView(View):
     def get(self,request,*args, **kwargs):
